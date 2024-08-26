1f337c6f9efbe55db062cb410768040834716d0c
yabuJunn <juancho04reyes@gmail.com>
2024-08-26 16:59:54 -0500

    Añadida logica de cambio de pantalla

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css'
-//import Start from './screens/Start/Start';
+import Start from './screens/Start/Start';
 import Second from './screens/Second/Second';
 
 const App: React.FC = () => {
-  return (
-    <div className="app">
-      <Second/>
-    </div>
-  );
+  const [pageId, setPageId] = useState("Second")
+
+  switch (pageId) {
+    case "Start":
+      return (
+        <div className="app">
+          <Start></Start>
+        </div>
+      );
+    case "Second":
+      return (
+        <div className="app">
+          <Second></Second>
+        </div>
+      );
+    default:
+      break;
+  }
+
+
 };
 
 export default App;
